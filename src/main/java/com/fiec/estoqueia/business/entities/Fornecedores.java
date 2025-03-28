74a25c684b1223ce21a4ae7f8bf48f0cd7825053
unknown <37408@fiecalunos.lan>
2025-03-27 21:58:50 -0300

    Primeiro commit

---
+++ src/main/java/com/fiec/estoqueia/business/entities/Fornecedores.java
@@ -0,0 +1,20 @@
+package com.fiec.estoqueia.business.entities;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "fornecedores")
+public class Fornecedores {
+
+    @Id
+    private ObjectId id;
+    private String nome;
+    private String cnpj;
+    private Endereco endereco; // Assumindo que Endereco é outra classe que você tem
+    private String telefone;
+    private String email;
+    private Boolean ativo;
+}
---
+++ src/test/java/com/fiec/estoqueia/EstoqueiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiec.estoqueia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
